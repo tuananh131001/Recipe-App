2fe2ad136ce0c3aa0012f84ced841ddfd1192baa
William <s3864077@rmit.edu.vn>
2022-06-29 14:02:19 +0700

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by William on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
