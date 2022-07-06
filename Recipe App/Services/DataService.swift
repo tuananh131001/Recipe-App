a37f4b7003ab2db1fa7d76779abfaaa33d89fa2f
William <s3864077@rmit.edu.vn>
2022-07-06 15:07:39 +0700

    implement enviroment

---
+++ Recipe App/Services/DataService.swift
@@ -0,0 +1,8 @@
+//
+//  DataService.swift
+//  Recipe App
+//
+//  Created by William on 06/07/2022.
+//
+
+import Foundation
---
+++ Recipe App/Views/RecipeFeaturedView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeFeaturedView.swift
+//  Recipe App
+//
+//  Created by William on 06/07/2022.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
